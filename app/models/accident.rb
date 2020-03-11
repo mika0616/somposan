9e733f92542aefafcda2a811cf21de9e45f0f77a
mika0616 <nokolight0616@gmail.com>
2020-03-11 18:45:50 +0900

    [create]事故登録機能を実装

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -1,2 +1,13 @@
 class Accident < ApplicationRecord
+	attachment :situation_img
+	
+	# アソシエーション
+	has_one :solution_report, dependent: :destroy
+	belongs_to :question_user
+
+	# バリデーション
+	with_options presence: true do
+		validates :title
+		validates :body
+	end
 end
