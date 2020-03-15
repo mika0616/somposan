e18e9623e1f00c62391a9216c7ca47b52830fdfe
mika0616 <nokolight0616@gmail.com>
2020-03-15 21:07:32 +0900

    マイグレーションファイルを削除

--- db/migrate/20200309082326_drop_table_question_users.rb
+++
@@ -1,5 +0,0 @@
-class DropTableQuestionUsers < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :question_users
-  end
-end
--- db/migrate/20200309082505_drop_table_answer_users.rb
+++
@@ -1,5 +0,0 @@
-class DropTableAnswerUsers < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :answer_users
-  end
-end
